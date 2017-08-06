71ff6bdfcd037a9573d5c2e02e65c371abd4928a
Flavio <flavio1605@bol.com.br>
2017-08-05 22:23:37 -0300

    melhora no fluxo

--- filtro.sh
+++ filtro.sh
@@ -1,6 +1,15 @@
-
 #!/bin/bash
-
 DIR=/root/archives
+arquivos_auterado_ultima_hora=`find $DIR -mmin -720 -name "*.services"  | cut -c16-18 > Log_Servico`
+seleciona_log=`cat /root/archives/Log_Servico | cut -c1-3`
 
-find $DIR -mmin -720 -name "*.services"  | cut -c16-18 > $DIR/Log_Servico
+function DeletaLinha {
+        sed -i '/000/d' /root/archives/Log_Servico
+}
+
+
+for i in $arquivos_auterado_ultima_hora ; do
+        sed -i '/000/d' /root/archives/Log_Servico
+
+done
+
--- lista.sh
+++ lista.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 DIR=/root/archives/
-teste=`tail -n1 /var/log/messages | grep Apache | cut -c8-15  >>  /root/archives/log.txt`
-for i in $(cat /root/archives/Log_Servico | cut -c1-3); do
-        systemctl stop httpd.service
-        $teste
-        systemctl start httpd.service
+seleciona_log=`cat /root/archives/Log_Servico | cut -c1-3`
 
-
-
-
-
-
-        #tail -n1 /var/log/messages | grep Apache | cut -c8-15  >>  /root/archives/log.txt
-
+for i in $seleciona_log ; do
+   systemctl stop httpd.service
+   systemctl start httpd.service
+   tail -n1 /var/log/messages | grep Apache | cut -c8-15 >>  /root/archives/log.txt
 
 done
-
 
